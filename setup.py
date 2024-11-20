e7d47959308a7ed7804370673874be53ee89adb0
can bingöl <114669146+canbingol@users.noreply.github.com>
2024-11-20 17:32:12 +0300

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medibot',
+    version= '0.0',
+    author= 'Can Bingöl',
+    author_email= 'canbingol36@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
