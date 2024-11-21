901bb922f0be684af2704df2a72bb7167769af47
can bingöl <114669146+canbingol@users.noreply.github.com>
2024-11-21 11:55:04 +0300

    project finished

---
+++ src/prompt.py
@@ -0,0 +1,12 @@
+
+
+
+system_prompt = (
+    "you are an assistant for question-answering tasks."
+    "use the following pieces retrieved context to answer"
+    "the question. If you don't know the answewr , say that you"
+    "don't know. Use three sentences maximum and peek the"
+    "answer concise"
+    "\n\n"
+    "{context}"
+)
